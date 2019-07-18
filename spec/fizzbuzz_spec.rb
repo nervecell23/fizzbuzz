4fbbee01fedef80777c118a0ed44d451069dc2f5
Chuan Cheng <c_chuan23@hotmail.com>
2019-07-18 13:33:17 +0100

    FizzBuzz quiz_completed

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,8 +1,35 @@
 require_relative '../lib/fizzbuzz'
 
-
 describe 'fizzbuzz' do
-  it 'returns "fizz" when passed 3' do
-    expect(fizzbuzz(3)).to eq 'fizz'
-  end
+
+  test_case = 1..10
+
+    test_case.each do |value|
+
+      condition = (value % 3 == 0 && value % 5 == 0)
+
+      it 'multiple of 3&5', :if => condition do
+        expect(fizzbuzz(value)).to eq 'fizzbuzz'
+      end
+
+      condition = (value % 3 == 0 && value % 5 != 0)
+
+      it 'multiple of 3', :if => condition do
+        expect(fizzbuzz(value)).to eq 'fizz'
+      end
+
+      condition = (value % 3 != 0 && value % 5 == 0)
+
+      it 'multiple of 5', :if => condition do
+        expect(fizzbuzz(value)).to eq 'buzz'
+      end
+
+      condition = (value % 3 != 0 && value % 5 != 0)
+
+      it 'others', :if => condition do
+        expect(fizzbuzz(value)).to eq value
+      end
+
+
+    end
 end
